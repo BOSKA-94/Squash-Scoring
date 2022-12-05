7bc278d2500725ffafb8528bdb665b2ebac5260f
Kiryl Baskaulau <k.baskaulau@reply.com>
2022-12-05 18:29:00 +0100

    Fixes

--- page/index.js
+++ page/index.js
@@ -1,15 +1,42 @@
 import * as hmUI from '@zos/ui'
 import { gettext } from 'i18n'
 import { log as Logger } from '@zos/utils'
-import { DEVICE_HEIGHT, DEVICE_WIDTH } from '../../calories/utils/styles-gts-3'
+import { px } from '@zos/utils'
+import { getDeviceInfo } from '@zos/device'
 
 Page({
   build() {
+    const text = hmUI.createWidget(hmUI.widget.TEXT, {
+      x: 96,
+      y: 120,
+      w: 288,
+      h: 46,
+      color: 0xffffff,
+      text_size: 12,
+      align_h: hmUI.align.LEFT,
+      align_v: hmUI.align.BOTTOM,
+      text_style: hmUI.text_style.NONE,
+      text: 'Player1'
+    })
+
+    const text2 = hmUI.createWidget(hmUI.widget.TEXT, {
+      x: 96,
+      y: 120,
+      w: 288,
+      h: 46,
+      color: 0xffffff,
+      text_size: 12,
+      align_h: hmUI.align.RIGHT,
+      align_v: hmUI.align.BOTTOM,
+      text_style: hmUI.text_style.NONE,
+      text: 'Player2'
+    })
+
     hmUI.createWidget(hmUI.widget.BUTTON, {
       x: 0,
       y: 0,
-      w: (DEVICE_WIDTH - px(233)),
-      h: DEVICE_HEIGHT,
+      w: (466 - px(233)),
+      h: 466,
       normal_color: 0x2c79cc,
       press_color: 0xfeb4a8,
       text: 'Player1',
@@ -26,8 +53,8 @@ Page({
     hmUI.createWidget(hmUI.widget.BUTTON, {
       x: 250,
       y: 0,
-      w: (DEVICE_WIDTH - px(233)),
-      h: DEVICE_HEIGHT,
+      w: (466 - px(233)),
+      h: 466,
       normal_color: 0xfc6950,
       press_color: 0xfeb4a8,
       text: 'Player2',
