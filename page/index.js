7c23535ccf6c448b0108cc63514656b760db24ad
Kiryl Baskaulau <k.baskaulau@reply.com>
2022-12-06 10:24:08 +0100

    Base concept

--- page/index.js
+++ page/index.js
@@ -6,65 +6,63 @@ import { getDeviceInfo } from '@zos/device'
 
 Page({
   build() {
-    const text = hmUI.createWidget(hmUI.widget.TEXT, {
-      x: 96,
-      y: 120,
-      w: 288,
-      h: 46,
-      color: 0xffffff,
-      text_size: 12,
-      align_h: hmUI.align.LEFT,
-      align_v: hmUI.align.BOTTOM,
-      text_style: hmUI.text_style.NONE,
-      text: 'Player1'
-    })
+    let count = 0
+    
+    // const text = hmUI.createWidget(hmUI.widget.TEXT, {
+    //   x: 96,
+    //   y: 120,
+    //   w: 288,
+    //   h: 46,
+    //   color: 0xffffff,
+    //   text_size: 12,
+    //   align_h: hmUI.align.LEFT,
+    //   align_v: hmUI.align.BOTTOM,
+    //   text_style: hmUI.text_style.NONE,
+    //   text: 'Player1'
+    // })
 
-    const text2 = hmUI.createWidget(hmUI.widget.TEXT, {
-      x: 96,
-      y: 120,
-      w: 288,
-      h: 46,
-      color: 0xffffff,
-      text_size: 12,
-      align_h: hmUI.align.RIGHT,
-      align_v: hmUI.align.BOTTOM,
-      text_style: hmUI.text_style.NONE,
-      text: 'Player2'
-    })
+    //this.buildTopContent(count)
+
+    // const text2 = hmUI.createWidget(hmUI.widget.TEXT, {
+    //   x: 96,
+    //   y: 120,
+    //   w: 288,
+    //   h: 46,
+    //   color: 0xffffff,
+    //   text_size: 12,
+    //   align_h: hmUI.align.RIGHT,
+    //   align_v: hmUI.align.BOTTOM,
+    //   text_style: hmUI.text_style.NONE,
+    //   text: 'Player2'
+    // })
 
     hmUI.createWidget(hmUI.widget.BUTTON, {
       x: 0,
       y: 0,
-      w: (466 - px(233)),
+      w: (466 - 233),
       h: 466,
       normal_color: 0x2c79cc,
-      press_color: 0xfeb4a8,
-      text: 'Player1',
+      press_color: 0x5895f0,
+      text: count,
+      text_size: 100,
+      color: 0xfc6950,
       click_func: (button_widget) => {
-        button_widget.setProperty(hmUI.prop.MORE, {
-          x: (480 - 400) / 2,
-          y: 300,
-          w: 400,
-          h: 100
-        })
+        this.count ++
       }
     })
 
     hmUI.createWidget(hmUI.widget.BUTTON, {
-      x: 250,
+      x: 234,
       y: 0,
-      w: (466 - px(233)),
+      w: (466 - 233),
       h: 466,
       normal_color: 0xfc6950,
       press_color: 0xfeb4a8,
-      text: 'Player2',
+      text: count,
+      text_size: 100,
+      color: 0x2c79cc,
       click_func: (button_widget) => {
-        button_widget.setProperty(hmUI.prop.MORE, {
-          x: (480 - 400) / 2,
-          y: 300,
-          w: 400,
-          h: 100
-        })
+        count ++
       }
     })
   }
